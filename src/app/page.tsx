974953ee9601c7554d12273eb8ec194029bc0509
Roy Koerner <royjkoerner@gmail.com>
2024-04-07 14:21:01 -0500

    blahblahblah

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1 className=" text-blue-950 text-5xl">Home Page</h1>
+      <h1 className="jwprimarycolor text-5xl">Home Page</h1>
       <div className="m-4 p-4 text- bg-pink-500 rounded-md bg-pink-900">
         we are the best of the best
       </div>
+      <div className="jwcontainer">hello my name is roy</div>
+      <div className="h-16 bg-slate-500">aiuwhfauhesf</div>
       <div className="flex flex-wrap gap-2 m-[10px] p-5 bg-teal-400">
         <picture>
           <img className="w-16 h-auto" src="/images/jwlogo1.png" />
