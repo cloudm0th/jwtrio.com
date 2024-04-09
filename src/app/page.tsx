33a157cf13c65cd533f9b4f53652dec0fcd508ed
Roy Koerner <royjkoerner@gmail.com>
2024-04-08 21:11:57 -0500

    first round of gettin shtuff here

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,77 +2,53 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h1 className="jwprimarycolor text-5xl">Home Page</h1>
-      <div className="m-4 p-4 text- bg-pink-500 rounded-md bg-pink-900">
-        we are the best of the best
+    <div className="rs-hide-scrollbars text-black bg-yellow-600 absolute left-0 top-0 h-[100dvh] w-full overflow-scroll flex flex-col items-center">
+      <div>
+        <div>
+          <picture>
+            <img className="w-16 h-auto" src="/images/jw-type-black.svg" />
+          </picture>
+        </div>
+        <div>
+          <picture>
+            <img className="w-16 h-auto" src="/images/jw-mark-black.svg" />
+          </picture>
+        </div>
       </div>
-      <div className="jwcontainer">hello my name is roy</div>
-      <div className="h-16 bg-slate-500">aiuwhfauhesf</div>
-      <div className="flex flex-wrap gap-2 m-[10px] p-5 bg-teal-400">
+      <div>
         <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
+          <img className="w-auto h-16" src="/images/exclamation-black.svg" />
         </picture>
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>{" "}
-        <picture>
-          <img className="w-16 h-auto" src="/images/jwlogo1.png" />
-        </picture>
+      </div>
+      <div className="bg-white border border-t-8 border-black grow">
+        <div>
+          <div>ABOUT THE JAZZARDOUS WASTE TRIO:</div>
+          <div>
+            We are Jazzardous Waste - A jazz trio made up of students from the
+            Oak Park & River Forest high school music program, native to the
+            Chicago land area.
+          </div>
+        </div>
+        <div>
+          <div>BOOK US:</div>
+          <div>
+            We are always looking to play for any parties, block parties,
+            restaraunts, etc. Please contact us for booking details!
+          </div>
+        </div>
+        <div>
+          <div>CONTACT US:</div>
+          <div>apoiwehfpu9awhefuh@bromail.gov (placeholder)</div>
+        </div>
+        <div>
+          <div>MEMBERS:</div>
+          <div>
+            <div>Roy Koerner - Piano</div>
+            <div>Braden Estanilla - Bass</div>
+            <div>Will Reinholdt - Drums</div>
+          </div>
+        </div>
+        <div>inst you email venmo</div>
       </div>
     </div>
   );
